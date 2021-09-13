2f753ac49b83aa386634434bdfd44b635a784a88
Fabian Brash <fabianbrash@gmail.com>
2021-09-13 19:41:32 -0400

    added advanced propTypes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import Greet from './components/Greet';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
+import Status from './components/Status';
+import Heading from './components/Heading';
+import Oscar from './components/Oscar';
 
 function App() {
   const personName = {
@@ -33,6 +36,15 @@ function App() {
        <Person name={personName} />
 
        <PersonList names={nameList} />
+
+       <Status status='error' />
+
+       <Heading>Placeholder text</Heading>
+
+       <Oscar>
+         <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
+       </Oscar>
+
       </header>
     </div>
   );
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 type GreetProps = {
     name: string,
-    messageCount: number,
+    messageCount?: number, //this is now optional
     isLoggedIn: boolean,
 }
 
 function Greet(props: GreetProps) {
+    const { messageCount = 0 } = props //if nothing is passed in set it to zero, if something is passed in set it to props
+    // note above we are also destructing messageCount hence we are using messageCount on line 15 instead of props.messageCount
     return (
         <div>
             {props.isLoggedIn ? (
-                <h2>Hello {props.name}, you have {props.messageCount} unread messages.</h2>
+                <h2>Hello {props.name}, you have {messageCount} unread messages.</h2>
             ) : (
                 <h2>Hello Guest, you are not logged in so no messages to retreive.</h2>
             )}
